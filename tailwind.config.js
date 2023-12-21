4bdff5173655c22f1c1e73d8dbe392da174a9b09
Luis Nogales <luis97.nogalesaaz@gmail.com>
2023-12-21 13:17:41 -0400

    segundo commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js", "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.php.blade",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
